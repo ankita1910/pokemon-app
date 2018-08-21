4ae39645ff27e809833bb1fad847a850d2b18895
Ankita <amasand23@gmail.com>
2018-08-20 19:16:19 -0700

    code cleanup

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,7 @@
 const express = require('express')
 const router = express.Router()
-const https = require('https')
 
 router.get('/', (req, res, next) => {
-
-    // https.get('http://pokeapi.co/api/v2/berry/1/', (response) => {
-    //     let data = ''
-    //     response.on('data', chunk => {
-    //         data += chunk
-    //         console.log('On Data', chunk)
-    //     })
-    //
-    //     response.on('end', () => {
-    //         console.log('Response', data)
-    //         console.log('API call done')
-    //     })
-    // })
     res.render('index')
 })
 
